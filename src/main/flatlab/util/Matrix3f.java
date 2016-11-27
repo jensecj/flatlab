06f1c60ce917f811331b267d3ed277216f0a64d5
Jens Jensen <jensecj@gmail.com>
2016-11-27 16:45:28 +0100

    added some more functionality to Matrix3f

--- src/main/flatlab/util/Matrix3f.java
+++ src/main/flatlab/util/Matrix3f.java
@@ -16,7 +16,8 @@ public class Matrix3f {
     public float _31, _32, _33;
 
     public static boolean eq(Matrix3f m1, Matrix3f m2)  {
-        return m1._11 == m2._11 &&
+        return
+            m1._11 == m2._11 &&
             m1._12 == m2._12 &&
             m1._13 == m2._13 &&
             m1._21 == m2._21 &&
@@ -41,6 +42,34 @@ public class Matrix3f {
         this._33 = _33;
     }
 
+    public static Matrix3f add(Matrix3f m1, Matrix3f m2) {
+        return new Matrix3f(m1._11 + m2._11, m1._12 + m2._12, m1._13 + m2._13,
+                            m1._21 + m2._21, m1._22 + m2._22, m1._23 + m2._23,
+                            m1._31 + m2._31, m1._32 + m2._32, m1._33 + m2._33);
+    }
+
+    public static Matrix3f add(Matrix3f m, float f) {
+        return new Matrix3f(m._11 + f, m._12 + f, m._13 + f,
+                            m._21 + f, m._22 + f, m._23 + f,
+                            m._31 + f, m._32 + f, m._33 + f);
+    }
+
+    public static Matrix3f sub(Matrix3f m1, Matrix3f m2) {
+        return new Matrix3f(m1._11 - m2._11, m1._12 - m2._12, m1._13 - m2._13,
+                            m1._21 - m2._21, m1._22 - m2._22, m1._23 - m2._23,
+                            m1._31 - m2._31, m1._32 - m2._32, m1._33 - m2._33);
+    }
+
+    public static Matrix3f sub(Matrix3f m, float f) {
+        return new Matrix3f(m._11 - f, m._12 - f, m._13 - f,
+                            m._21 - f, m._22 - f, m._23 - f,
+                            m._31 - f, m._32 - f, m._33 - f);
+    }
+
+    public static float sum(Matrix3f m) {
+        return m._11 + m._12 + m._13 + m._21 + m._22 + m._23 + m._31 + m._32 + m._33;
+    }
+
     public static Matrix3f mul(Matrix3f m1, Matrix3f m2) {
         return new Matrix3f((m1._11 * m2._11) + (m1._12 * m2._21) + (m1._13 * m2._31),
                             (m1._11 * m2._12) + (m1._12 * m2._22) + (m1._13 * m2._32),
@@ -53,6 +82,25 @@ public class Matrix3f {
                             (m1._31 * m2._11) + (m1._32 * m2._21) + (m1._33 * m2._31),
                             (m1._31 * m2._12) + (m1._32 * m2._22) + (m1._33 * m2._32),
                             (m1._31 * m2._13) + (m1._32 * m2._23) + (m1._33 * m2._33));
+    }
+
+    public static Matrix3f mul(Matrix3f m, float scalar) {
+        return new Matrix3f(m._11 * scalar, m._12 * scalar, m._13 * scalar,
+                            m._21 * scalar, m._22 * scalar, m._23 * scalar,
+                            m._31 * scalar, m._32 * scalar, m._33 * scalar);
+    }
+
+    public static Matrix3f mul(Matrix3f m, Vector2f v) {
+        Matrix3f t = new Matrix3f(v.x, 0, 0,
+                                  0, v.y, 0,
+                                  0, 0, 1);
+        return mul(m, t);
+    }
+
+    public static Matrix3f transpose(Matrix3f m) {
+        return new Matrix3f(m._11, m._21, m._31,
+                            m._12, m._22, m._32,
+                            m._13, m._23, m._33);
     }
 
     public static Matrix3f translate(Matrix3f m, Vector2f v) {
@@ -88,6 +136,8 @@ public class Matrix3f {
     }
 
     public static Matrix3f rotate(Matrix3f m, Vector2f forward, Vector2f side) {
+        // forward = Vector2f.normalize(forward);
+        // side = Vector2f.normalize(side);
         Matrix3f mat = new Matrix3f(forward.x, forward.y, 0,
                                     side.x,    side.y,    0,
                                     0,         0,         1);
